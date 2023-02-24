ef3865c6fdf4d7f2064a228bd105034c8d697bcb
al-chang <aac6692@gmail.com>
2023-02-24 13:31:54 -0500

    Fix geojsons, add dev log

---
+++ LOG.md
@@ -0,0 +1,17 @@
+# Development log
+
+Purpose of this file is to log the work and effort that went into developing the final product. This project requires a lot of learning for me, so I want to remember the steps it took to get me to the end.
+
+## 2/23/2023 - Data Collection
+
+The goal of this project is initially to create an interactive map of Boston. Since Blue Bike stations span all across the greater Boston area I needed to create a map file that would represent the region. Through research, I learned the easiest way to do this would be using the geojson file format. However I quickly ran into obstacles when I could not easily find existing geoJSONs for Boston's neighboring cities. Enter the OpenStreetMaps API.
+
+Following advice found in [this stackoverflow post](https://gis.stackexchange.com/questions/183248/getting-polygon-boundaries-of-city-in-json-from-google-maps-api), it is possible to extract a geoJSON from OpenStreetMaps for any city that they have defined boundaries of. This made it very easy to collect geoJSONs.
+
+## 2/24/2023 - Map Data Cleaning
+
+After acquiring individual geoJSONs I needed to combine them into one large geoJSON for the greater Boston area. The online tool I found to do this, ([findthatpostcode.co.uk](https://findthatpostcode.uk/tools/merge-geojson)) only accepted geoJSONs that were of the type `FeatureCollection`, but OpenStreetMaps gives geoJSONs in the `GeometryCollection` format. It is actually a simple manual process to convert between the two types (allowed by the fact that OpenStreetMaps geoJSONs are very simple in structure). So I manually converted them to the desired format and created my combined file.
+
+Using [this medium post](https://medium.com/@ivan.ha/using-d3-js-to-plot-an-interactive-map-34fbea76bd78) as a basis for my code and the help of ChatGPT to upgrade the code from `d3 v4` to `d3 v6` I was able to create a simple rendering of my greater Boston area geoJSON. The only problem was that some of the neighborhoods were inverted, resulting in the entire map being covered in a single color rather than just the neighborhood.
+
+This problem had me stumped for quite some time. I entered my geoJSON into several online resources and it appeared to render correctly in all of them. I dug a little deeper and found out that d3 actually cares about the ordering of the coordinates, whereas standard geoJSON processors do not. To fix the issue it was as simple as reversing the coordinate ordering in the geoJSON, which I could accomplish using [this online tool](https://observablehq.com/@bumbeishvili/rewind-geojson).
--- main.js
+++ main.js
@@ -1,36 +1,36 @@
 // Constants
-const FRAME_HEIGHT = 500;
-const FRAME_WIDTH = 500;
-const MARGINS = { left: 70, right: 50, top: 50, bottom: 50 };
 
-// Variables for testing
+const MAP_FINAL_URL = "/data/map-final/greaterboston.geojson";
+
+const geoJsonResponse = await fetch(MAP_FINAL_URL);
+const geoJson = await geoJsonResponse.json();
+
 const WIDTH = window.innerWidth;
 const HEIGHT = window.innerHeight;
-// End variables for testing
-
-const VIS_HEIGHT = FRAME_HEIGHT - MARGINS.top - MARGINS.bottom;
-const VIS_WIDTH = FRAME_WIDTH - MARGINS.left - MARGINS.right;
-
+const ZOOM_THRESHOLD = [0.3, 7];
 const OVERLAY_MULTIPLIER = 10;
 const OVERLAY_OFFSET = OVERLAY_MULTIPLIER / 2 - 0.5;
+const ZOOM_DURATION = 500;
+const ZOOM_IN_STEP = 2;
+const ZOOM_OUT_STEP = 1 / ZOOM_IN_STEP;
+const HOVER_COLOR = "#d36f80";
 
-const ZOOM_THRESHOLD = [0.3, 7];
-
-// const ROOT = JSON.parse(boston);
-
-// Event handlers
-const zoomHandler = () => g.attr("transform", d3.event.transform);
-
+// --------------- Event handler ---------------
 const zoom = d3.zoom().scaleExtent(ZOOM_THRESHOLD).on("zoom", zoomHandler);
 
-const BOSTON_MAP = d3
+function zoomHandler(event) {
+  g.attr("transform", event.transform);
+}
+
+// Prep svg
+const svg = d3
   .select("#boston-map")
   .append("svg")
-  .attr("height", "100%")
   .attr("width", "100%")
-  .attr("class", "frame");
+  .attr("height", "100%");
 
-const g = BOSTON_MAP.call(zoom).append("g");
+const g = svg.call(zoom).append("g");
+
 g.append("rect")
   .attr("width", WIDTH * OVERLAY_MULTIPLIER)
   .attr("height", HEIGHT * OVERLAY_MULTIPLIER)
@@ -41,21 +41,29 @@ g.append("rect")
   .style("fill", "none")
   .style("pointer-events", "all");
 
+// Align projection
 const projection = d3
   .geoMercator()
-  .center([42.3601, -71.0589])
+  .center([-71.0589, 42.3601])
   .scale(80000)
   .translate([WIDTH / 2, HEIGHT / 2]);
 
+// Prepare svg
 const path = d3.geoPath().projection(projection);
-// const color = d3.scaleOrdinal(d3.schemeCategory10.slice(1, 4));
-console.log(boston);
-g.append("g")
-  .selectAll("path")
-  .data(boston.features)
-  .enter()
-  .append("path")
-  .attr("d", path)
-  .attr("fill", (d, i) => "blue")
-  .attr("stroke", "#FFF")
-  .attr("stroke-width", 0.5);
+const color = d3.scaleOrdinal(d3.schemeCategory10);
+
+// Plot geojson
+renderMap(geoJson);
+
+function renderMap(root) {
+  // Draw districts and register event listeners
+  g.append("g")
+    .selectAll("path")
+    .data(root.features)
+    .enter()
+    .append("path")
+    .attr("d", path)
+    .attr("fill", (d, i) => color(i))
+    .attr("stroke", "#FFF")
+    .attr("stroke-width", 0.5);
+}
